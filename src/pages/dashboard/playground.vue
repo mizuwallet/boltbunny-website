6f64b878259910d91a93b3492517e4f436b51ae2
ice <ice@alcove.pro>
2024-04-18 12:15:47 +0800

    feat: playground

--- src/pages/dashboard/playground.vue
+++ src/pages/dashboard/playground.vue
@@ -1,10 +1,21 @@
 <template>
+  <h2>SDK Function Test</h2>
+  <h3 class="mt-5">Fuzz::mint</h3>
   <Button @click="sdkTest">fuzz::mint</Button>
+
+  <div class="flex-col gap-2 my-3 max-w-200 mt-5">
+    <h3>Custome Function</h3>
+    <Input v-model="custome.function" />
+    <Input v-model="custome.secretKey" placeholder="API Secret key" />
+    <Input v-model="custome.arguments" placeholder="Arguments, e.g. arg_1,args2" />
+    <Button @click="cumstomSDKTest">Approve</Button>
+  </div>
 </template>
 
 <script lang="ts" setup>
   import { AptosClient } from '@/config';
   import Button from '@/lib/Button.vue';
+  import Input from '@/lib/Input.vue';
   import useAppStore from '@/store/AppStore';
   import { SimpleTransaction } from '@aptos-labs/ts-sdk';
   import { message } from 'ant-design-vue';
@@ -18,7 +29,6 @@
 
   const appStore = useAppStore();
   const sdkTest = async () => {
-    console.log(appStore.address);
     if (!appStore.address) return;
 
     const txn: SimpleTransaction = await AptosClient.transaction.build.simple({
@@ -42,7 +52,7 @@
     console.log(txn.rawTransaction);
 
     try {
-      await GQL_Client.request(
+      const result: any = await GQL_Client.request(
         gql`
           mutation submitTransaction($signature: String!, $transaction: String!) {
             submitTransaction(txnParams: { signature: $signature, transaction: $transaction }) {
@@ -55,6 +65,57 @@
           transaction: txn.rawTransaction.bcsToHex().toString(),
         },
       );
+      alert(JSON.stringify(result));
+    } catch (e: any) {
+      console.log(e);
+      message.error(e.response.errors[0].message);
+    }
+  };
+
+  const custome = ref<any>({
+    function: functionId,
+    secretKey: '',
+    arguments: '',
+  });
+  const cumstomSDKTest = async () => {
+    if (!appStore.address) return;
+
+    const txn: SimpleTransaction = await AptosClient.transaction.build.simple({
+      sender: appStore.address,
+      data: {
+        function: custome.value.function,
+        typeArguments: [],
+        functionArguments: custome.value.arguments
+          .split(',')
+          .map((arg: string) => arg.trim())
+          .filter((arg: string) => arg),
+      },
+      withFeePayer: true,
+    });
+
+    const signature = await appStore.walletCore.signTransaction(txn);
+
+    const GQL_Client = new GraphQLClient(import.meta.env.VITE_APP_GRAPHQL_URL, {
+      headers: () => ({
+        'x-alcove-app-secret': custome.value.secretKey,
+      }),
+    });
+
+    try {
+      const result: any = await GQL_Client.request(
+        gql`
+          mutation submitTransaction($signature: String!, $transaction: String!) {
+            submitTransaction(txnParams: { signature: $signature, transaction: $transaction }) {
+              transactionHash
+            }
+          }
+        `,
+        {
+          signature: signature.bcsToHex().toString(),
+          transaction: txn.rawTransaction.bcsToHex().toString(),
+        },
+      );
+      alert(JSON.stringify(result));
     } catch (e: any) {
       console.log(e);
       message.error(e.response.errors[0].message);
