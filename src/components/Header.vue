cdd305f28a765b65a868445869fd8755d4308734
rebecca <r4058736@gmail.com>
2024-05-12 01:37:37 +0800

    refactor: index

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="w-full min-h-12 py-4 bg-primary/10">
-    <div class="container mx-auto flex gap-10 items-center">
-      <span class="text-2xl font-bold">Bolt Bunny</span>
-      <!-- <nav class="flex items-center gap-4">
-        <router-link to="/">Home</router-link>
-        <router-link to="/about">About</router-link>
-        <router-link to="/contact">Contact</router-link>
-      </nav> -->
+  <div class="header">
+    <div class="container mx-auto flex gap-12 items-center">
+      <nav class="hidden md:flex items-center uppercase ">
+        <router-link :to="item.link" v-for="item  in navList">{{ item.title }}</router-link>
+      </nav>
+      <div class="flex-center md:hidden w-10 h-10" @click="openMenu = true">
+        <font-awesome-icon icon="fa-solid fa-bars" class="cursor-default"></font-awesome-icon>
+      </div>
       <div class="ml-auto flex gap-4 items-center">
         <ConnectButton></ConnectButton>
       </div>
     </div>
+    <a-drawer v-model:open="openMenu" width="60%" placement="left" style="background-color: rgba(30, 34, 36, .9);">
+      <nav class="flex flex-col gap-20 uppercase ">
+        <router-link :to="item.link" v-for="item  in navList">{{ item.title }}</router-link>
+      </nav>
+    </a-drawer>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
 
+const navList = [{ title: 'How Does it Work?', link: '/' }, { title: 'Why Bolt Buuny?', link: '/' }, { title: 'Documentation', link: '/' }]
+
+const openMenu = ref(false);
+
+</script>
+
+<style lang="less" scoped>
+.header {
+  @apply w-full flex flex-wrap items-center min-h-25 py-4 fixed top-0 left-0 right-0 z-50;
+  background: rgba(30, 34, 36, .6);
+
+  a {
+    @apply flex-center w-60 h-8 rd-10 text-4 text-white;
+    border: 1px solid transparent;
+
+    &:hover {
+      border: 1px solid #94F69E;
+    }
+  }
+}
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,25 +1,169 @@
 <template>
   <div class="index-page">
-    <h1 class="centre-title">Why Bolt Buuny?</h1>
-    <section></section>
-    <h1 class="centre-title">How Does it Work?</h1>
-    <section></section>
-    <h1 class="centre-title">Documentation</h1>
-    <section></section>
+    <div class="landing-section">
+      <div class="img-box left-0">
+        <img class="absolute top-0" src="@/assets/images/index/pattern_blue1.png" alt="" />
+        <img class="absolute top-0%" src="@/assets/images/index/pattern_green1.png" alt="" />
+        <img class="absolute top-60%" src="@/assets/images/index/pattern_pink_1.png" alt="" />
+        <img class="absolute bottom-0" src="@/assets/images/index/pattern_green3.png" alt="" />
+      </div>
+      <div class="landing-content">
+        <img class="w-full max-w-890px" src="@/assets/images/index/boltbunny_logo.png" alt="" />
+        <h1 class="title">{{ title }}</h1>
+        <h2 class="sub-title">{{ subTitle }}</h2>
+        <div class="flex-center mt-10">
+          <a href="">
+            <button>start building on Aptos</button>
+          </a>
+        </div>
+      </div>
+      <div class="img-box right-0">
+        <img class="absolute top-0" src="@/assets/images/index/pattern_green2.png" alt="" />
+        <img class="absolute top-25%" src="@/assets/images/index/pattern_pink_2.png" alt="" />
+        <img class="absolute top-50%" src="@/assets/images/index/pattern_green4.png" alt="" />
+        <img class="absolute bottom-0 right-10%" src="@/assets/images/index/pattern_blue2.png" alt="" />
+      </div>
+    </div>
+    <div class="imfo-section">
+      <div class="imfo-title">{{ imfoTitle }}</div>
+      <ul class="imfo-list">
+        <li v-for="item in imfoList">
+          <div class="icon">
+            <img :src="item.icon" :alt="item.title" />
+          </div>
+          <div class="title">{{ item.title }}</div>
+          <div class="sub-title">{{ item.subTitle }}</div>
+          <div class="border-bottom"></div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import icon1 from '@/assets/images/index/icon_01.png';
+import icon2 from '@/assets/images/index/icon_02.png';
+import icon3 from '@/assets/images/index/icon_03.png';
+
+const title = 'Powerful Developer Infrastructure for Aptos Builders';
+const subTitle =
+  'We offer Aptos development tools to reduce costs and lower entry barriers, boosting efficiency and supporting the Aptos ecosystem.';
+
+const imfoTitle = 'Start Building on Aptos with the sponsored transactions service';
+const imfoList = [
+  {
+    icon: icon1,
+    title: 'Instant Setup',
+    subTitle:
+      'Integrate effortlessly with just one line of code, dramatically simplifying your development process and minimizing time to market.',
+  },
+  {
+    icon: icon2,
+    title: 'Precision Control',
+    subTitle:
+      'Employ our advanced permission management systemto secure and tailor your transaction processes,enhancing operational integrity.',
+  },
+  {
+    icon: icon3,
+    title: 'Rich Insights',
+    subTitle:
+      'Capitalize on detailed BI analytics for a comprehensive understanding of  transaction dynamics, supporting informed decision-making and strategy refinement.',
+  },
+  {
+    icon: icon2,
+    title: 'Optimized Security',
+    subTitle:
+      'Ensure the utmost security and efficiency with our service that guarantee fast and reliable transaction execution',
+  },
+];
+</script>
 
 <style lang="less" scoped>
-  .centre-title {
-    @apply m-0;
-    @apply text-4xl lg:text-6xl text-center font-bold !leading-30;
+.index-page {
+  @apply flex-col w-full pt-25;
+
+  .landing-section {
+    @apply bg-[#1E2224] flex relative overflow-hidden;
+
+    .img-box {
+      @apply absolute bottom-0 top-0 w-full z-1;
+      max-width: 22%;
+    }
+
+    .landing-content {
+      @apply py-20 px-4 m-auto max-w-230 z-11;
+      @apply md:pb-40;
+
+      .title {
+        @apply text-4 font-900 text-white text-center leading-32px;
+        @apply md:text-8 md:leading-50px md:mt-[-20px];
+        letter-spacing: 4px;
+        text-transform: uppercase;
+      }
+
+      .sub-title {
+        @apply max-w-640px mx-auto text-4 font-400 text-white text-center leading-[40px] mt-5;
+        @apply md:text-18px;
+        letter-spacing: 2px;
+      }
+
+      a {
+        @apply font-800 text-4 text-center px-6 py-2 rd-10 inline-block;
+        @apply md:text-7 md:px-16 md:py-4;
+        border: 5px solid #94F69E;
+
+        button {
+          text-transform: uppercase;
+          letter-spacing: 2px;
+        }
+      }
+    }
+
   }
 
-  .index-page {
-    @apply flex-col w-full;
+  .imfo-section {
+    @apply bg-[#F2F2F2] w-full px-4;
+
+    .imfo-title {
+      @apply text-6 text-center font-900 text-[#1E2224] pt-6;
+      @apply md:pt-14 md:pb-6;
+    }
+
+    .imfo-list {
+      @apply flex flex-wrap;
+      @apply md:max-w-880px xl:max-w-[1080px];
+      margin: 80px auto 0;
+
+      li {
+        @apply bg-[#1e2224] text-center relative rd-30px;
+        @apply w-100 p-12 mb-20 mx-auto;
+        @apply md:mb-30;
+
+        .icon {
+          @apply absolute w-30 h-30 bg-[#6DFB93] flex-center top-[-60px] left-50% translate-x-[-50%] rd-[50%];
+
+          img {
+            @apply w-75px h-75px;
+          }
+        }
+
+        .title {
+          @apply text-5 text-[#6DFB93] font-800 mt-6 mb-2;
+        }
+
+        .sub-title {
+          @apply text-4 text-white font-500 leading-30px;
+        }
+
+        .border-bottom {
+          @apply h-25px w-full absolute bottom-0 left-0 right-0;
+          @apply bg-[#6DFB93];
+          border-radius: 0 0 30px 30px;
+        }
+      }
+    }
   }
+}
 </style>
 
 <route lang="yaml">
